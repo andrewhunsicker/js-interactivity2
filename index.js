759d40ca935985606ec4dcb3d0471eb541daba7b
andrew hunsicker <ahunsicker888@gmail.com>
2022-04-06 18:08:59 -0400

    finished basic lab will do intermidiate later

---
+++ index.js
@@ -0,0 +1,45 @@
+let message = document.querySelector(`#message`)
+
+let form = document.querySelector('form')
+const addMovie = (event) => {
+    event.preventDefault()
+    let inputField = document.querySelector("input");
+
+let movie = document.createElement(`li`);
+let movieTitle = document.createElement(`span`)
+movieTitle.textContent = inputField.value
+movieTitle.addEventListener(`click`, crossOffMovie)
+
+movie.appendChild(movieTitle)
+document.querySelector(`ul`).appendChild(movie)
+
+let deleteBtn = document.createElement('button')
+deleteBtn.textContent = 'x'
+deleteBtn.addEventListener(`click`, deleteMovie)
+movie.appendChild(deleteBtn)
+
+inputField.vaule = ``
+
+  return document.querySelector(`ul`).appendChild(movie)
+}
+
+const deleteMovie = (event) => {
+    event.target.parentNode.remove()
+    message.textContent = `Movie deleted!`
+}
+
+const crossOffMovie= (event) => {
+    event.target.classList.toggle('checked')
+    if(event.target.classList.contains(`checked`)){
+        message.textContent = `Movie watched!`
+    }else{
+        message.textContent = `Movie re-added`
+    }
+}
+
+
+
+
+
+
+form.addEventListener(`submit`, addMovie)
